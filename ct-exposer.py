f9e64509ec61476557a49af020bcec71af95e3c0
chris408 <chris408@users.noreply.github.com>
2018-09-03 21:38:29 -0700

    First.

---
+++ ct-exposer.py
@@ -0,0 +1,77 @@
+import sys
+import requests
+import urllib3
+import re
+import gevent
+from gevent import socket
+from gevent.pool import Pool
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+requests.packages.urllib3.disable_warnings()
+                
+domain = sys.argv[1]
+domainsFound = {} 
+domainsNotFound = {}
+
+def main(domain):
+    global domainsFound, domainsNotFound
+    print("[+]: Downloading domain list...")
+    response = collectResponse(domain)
+    print("[+]: Download of domain list complete.")
+    domains = collectDomains(response)
+    print("[+]: Parsed %s domain(s) from list." % len(domains))
+    
+    pool = Pool(15)
+    for domain in domains:
+        pool.spawn(resolve, domain)
+    pool.join(timeout=1)
+
+    print("\n[+]: Domains found:")
+    printDomains(domainsFound)
+    print("\n[+]: Domains with no DNS record:")
+    printDomains(domainsNotFound)
+
+def resolve(domain):
+    global domainsFound, domainsNotFound
+    try:
+        domainsFound[domain] =  socket.gethostbyname(domain)
+    except:
+        domainsNotFound[domain] = 'none'
+
+def printDomains(domains):
+    for domain in domains:
+        print("%s\t%s" % (domains[domain], domain))
+
+def collectResponse(domain):
+    headers = {'Host': 'ctsearch.entrust.com',
+	       'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36',
+	       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+	       'Accept-Language': 'en-US,en;q=0.5',
+	       'Accept-Encoding': 'gzip, deflate',
+               'Referer': 'https://www.entrust.com/ct-search/',
+	       'Connection': 'close',
+	       'Upgrade-Insecure-Requests': '1',
+	       'Content-Length': '0'}
+
+    url = 'https://ctsearch.entrust.com/api/v1/certificates?fields=subjectDN&domain=' + domain + '&includeExpired=true&exactMatch=false&limit=5000'
+    response = requests.get(url, headers=headers, verify=False)
+    #print(response.status_code)
+    #print(response.text)
+    return response
+
+def collectDomains(response):
+    domains = []
+    restring = re.compile(r"cn\\u003d(.*?)(\"|,)", re.MULTILINE)
+    match = re.findall(restring, response.text)
+    if match:
+        for domain in match:
+            #The following line is meant to avoid adding wildcard domains, as these will not resolve.
+            if((domain[0] not in domains) and not (re.search("^\*\.", domain[0]))):
+                domains.append(domain[0])
+    return domains
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+    	print("Usage: python ct-exposer.py domain.com")
+    	sys.exit(1) 
+    main(domain)
